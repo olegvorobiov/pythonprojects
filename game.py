057bc4bf18f8984c4f8a1bf5d3bdfc077373e7c4
root <root@ansible.local>
2020-03-30 21:37:18 -0400

    added new changes

--- game.py
+++ game.py
@@ -1,3 +1,12 @@
 #Author: Oleg Vorobiov
 #Contact: olegvorobyov90@gmail.com
 #A fun little python game
+
+#name = input("What is your name?")
+#age = input("What your age?")
+#your_age_in_hundred_years = int(age) + 100
+#print("Hi my name is " + name + "!")
+#print("Your age is " + str(age))
+#print("In 100 years ypu will be " + str(your_age_in_hundred_years))
+while True:
+    print("Hello")
